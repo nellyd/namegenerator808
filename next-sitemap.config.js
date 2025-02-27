bd382540696dc4f9c97fde45e6f74fe4d4de0120
NellyD <neal@evokeseo.co.uk>
2025-02-27 20:44:28 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://namegeno.com', // Replace with your actual domain
+    generateRobotsTxt: true,
+    // Other options:
+    // changefreq: 'daily',
+    // priority: 0.7,
+    // exclude: ['/admin/*', '/private/*'],
+  }
